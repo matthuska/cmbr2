468b481222add3094e9ce11cb36fe975644a5ad4
your-highness <helmuth@molgen.mpg.de>
2013-02-28 16:09:15 +0100

    Added CpG odd ratio function stub

--- norm-cpg.R
+++ norm-cpg.R
@@ -11,3 +11,21 @@ norm_cpg <- function(seqs) {
     ncpg <- dnf[,"CG"] / pmax(0.00000000001, (((nf[,"G"] + nf[,"C"]) / 2)^2))
     return(ncpg)
 }
+
+#' Computes GC content and CpGodd for a given GRanges object
+#'
+#' As a threshold between HCP and LCP a CpG odd ratio of .4 is a good approximate.
+#'
+#' You can construct a 3kb-flanking-TSS representation for a given set of granges with:
+#'
+#'    require(BSgenome)
+#'    require(BSgenome.Hsapiens.UCSC.hg19)
+#'    require(GenomicFeatures)
+#'    granges.tss = flank(granges, 1500, start=T)
+#'    
+#'    computeCpGOddInGRanges( Hsapiens, granges, tss)
+#'
+computeCpGOddInGRanges <- function(Hsapiens=Hsapiens, granges=granges) {
+
+}
+
