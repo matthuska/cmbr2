9a64d2f1e4b7ad7bb21fd0bbe85d8736e8ad758d
Alessandro Mammana <mammana@antipasti.molgen.mpg.de>
2014-12-23 14:01:23 +0100

    Removed the bamsignals content from cmbr (I'm going to create a separate github repository for it)

--- bamsignals_INSTALL.R
+++
@@ -1,8 +0,0 @@
-# run it using "R(the R version you are using) --slave < bamsignals_INSTALL.R"
-# Make sure you are in the same directory where bamsignals_INSTALL.R
-# and the directory bamsignals/ are.
-# After that the package is installed, to use it just do
-# from within R "library(bamsignals)".
-system("R CMD build bamsignals")
-install.packages("bamsignals_1.1.tar.gz")
-system("rm bamsignals_1.1.tar.gz")
